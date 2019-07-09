3cca3477fcbbd68ca7eb0a5c649b1398c9a6fc87
theonestenza <theonestenza@gmail.com>
2019-07-09 10:43:37 +0200

    [ft-#166998569]-home page refactor

--- server/index.js
+++ server/index.js
@@ -1,32 +1,22 @@
 import express from 'express';
-import Joi from 'joi';
 import bodyParser from 'body-parser';
-import User from './controllers/usercontrollers';
-import Property from  './controllers/propertycontrollers';
-import Flag from './controllers/flagcontrollers';
+import userroutes from './routes/userroutes';
+import propertyroutes from  './routes/propertyroutes';
+import flagroutes from './routes/flagroutes';
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // users
-app.post('/api/v1/users', User.create);
-app.get('/api/v1/users', User.getAll);
-app.get('/api/v1/users/:id', User.getOne);
-app.put('/api/v1/users/:id', User.update);
-app.delete('/api/v1/users/:id',User.delete);
+app.use('/api/v1/users', userroutes);
+
 // property
-app.post('/api/v1/properties', Property.create);
-app.get('/api/v1/properties', Property.getAll);
-app.get('/api/v1/properties/:id', Property.getOne);
-app.put('/api/v1/properties/:id', Property.update);
-app.delete('/api/v1/properties/:id',Property.delete);
+app.use('/api/v1/properties', propertyroutes);
+
 // flag
-app.post('/api/v1/flags', Flag.create);
-app.get('/api/v1/flags', Flag.getAll);
-app.get('/api/v1/flags/:id', Flag.getOne);
-app.put('/api/v1/flags/:id', Flag.update);
-app.delete('/api/v1/flags/:id',Flag.delete);
+app.use('/api/v1/flags', flagroutes);
+
 app.get('/', (req, res) => {
     return res.status(200).send({'message': 'welcome to property pro lite,we are happy to here from you'});
   })
